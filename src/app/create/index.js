a8cc038cb50027d6da5fa2969c9e71d44403778d
ZamKaf <chadov@phystech.edu>
2018-10-24 02:10:01 +0300

    some test changes

--- src/app/create/index.js
+++ src/app/create/index.js
@@ -1,2 +1,6 @@
 import FormInput from '../../lib/components/form/-input';
 import MessageForm from '../../lib/components/message-form';
+
+import say from '../../lib/test'; // ES-module
+alert("wer1");
+say('Javascript');
