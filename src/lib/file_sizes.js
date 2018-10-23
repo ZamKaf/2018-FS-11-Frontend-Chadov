d3b8e87c2c81f88114b5abc8102355073ab9711f
ZamKaf <chadov@phystech.edu>
2018-10-24 01:46:52 +0300

    adde constatnts export file. added project with message form

---
+++ src/lib/file_sizes.js
@@ -0,0 +1,15 @@
+export const KB = 1024;
+export const Kb = KB / 8;
+export const MB = KB * 1024;
+export const Mb = MB / 8;
+export const GB = MB * 1024;
+export const Gb = GB / 8;
+
+export function GetReadableSize(size) {
+  if (size > GB) return `${size / GB | 0} GBytes`;
+  if (size > MB) return `${size / MB | 0} MBytes`;
+  if (size > KB) return `${size / KB | 0} KBytes`;
+  return `${size} Bytes`;
+}
+
+// export { KB, Kb, MB, Mb, GB, Gb }
