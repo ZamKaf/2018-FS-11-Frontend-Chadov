998326e63a72a988b59bd57a5979ce30276a8ecf
ZamKaf <chadov@phystech.edu>
2018-11-15 02:13:11 +0300

    all

--- src/App.js
+++ src/App.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TextBrowser from './TextBrowser.js';
+import StartPage from './StartPage.js'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <TextBrowser></TextBrowser>
-          {/*
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-*/}
+        <Router>
+          <div className="App">
+              <Route path='/' exact component={StartPage}/>
+              <Route path='/:name' exact component={TextBrowser} />
+              {/*
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+    */}
 
-      </div>
+          </div>
+        </Router>
     );
   }
 }
