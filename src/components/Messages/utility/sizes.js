82c31e47fb815b7a18c298feb4b63d3933261195
ZamKaf <chadov@phystech.edu>
2019-04-22 01:14:24 +0300

    fix <0

--- src/components/Messages/utility/sizes.js
+++ src/components/Messages/utility/sizes.js
@@ -6,13 +6,17 @@ export const GB = MB * 1024;
 export const Gb = GB / 8;
 
 export const GetReadableSize = (size) => {
-  if (!isNumeric(size)) return false;
+  if (!isValid(size)) return false;
+
   if (size >= GB) return `${size / GB | 0} GBytes`;
   if (size >= MB) return `${size / MB | 0} MBytes`;
   if (size >= KB) return `${size / KB | 0} KBytes`;
   return `${size} Bytes`;
 }
 
-function isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+function isValid(n) {
+    if (!(!isNaN(parseFloat(n)) && isFinite(n))) return false;
+    if (n < 0) return false;
+
+    return true;
 }
